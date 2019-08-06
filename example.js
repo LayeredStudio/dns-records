5edc6c2fe5ff5fb70540a4cc92dc3f8a95828602
Andrei <andrei@laye.red>
2019-08-06 17:29:56 +0200

    Updated - example with more code

--- example.js
+++ example.js
@@ -7,6 +7,10 @@ const dnsRecords = require('./index.js');
 		const records = await dnsRecords.getDnsRecords('cloudflare.com', 'TXT')
 		console.log('DNS TXT records', records)
 
+		// Get NameServers info
+		const ns = await dnsRecords.getNameServers('cloudflare.com')
+		console.log('NameServers', ns)
+
 		// Discover all DNS records
 		const allRecords = await dnsRecords.getAllRecords('x.com')
 		console.log('DNS all records', allRecords)
